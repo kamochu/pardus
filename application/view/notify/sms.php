7ae80857e83cdeb650dc9fc00e7e17d1182d6fab
Samuel Kamochu <kamochu@gmail.com>
2015-04-11 16:30:08 +0300

    Modified the notify sms view to deal with repeated parameters and removed the namespace from tagname.

--- application/view/notify/sms.php
+++ application/view/notify/sms.php
@@ -7,18 +7,21 @@
 	
 	//global declaration
 	global 	$parameters;
+	global 	$repeated_parameters;
 	global 	$current_param;
 	global 	$current_value;
 	
 	//initialize the global parameters
 	$parameters = array();
+	$repeated_parameters = array();
 	$current_param = "";
 	$current_value = "";
 	
 	/*
 	* The method called when a start tag is encountered.
 	*/
-	function sax_start($sax, $tag, $attr) {
+	function sax_start($sax, $tag, $attr) 
+	{
 		global $current_param;
 		$current_param = $tag; // copy the tag name into current parameter attribute
 	}
@@ -26,14 +29,38 @@
 	/*
 	* The method called when a end tag is encountered.
 	*/
-	function sax_end($sax, $tag) {
+	function sax_end($sax, $tag) 
+	{
 		global $current_param;
 		global $current_value;
 		global $parameters;
-		
+		global $repeated_parameters;
+
 		//if they are equal
-		if(strcmp($current_param, $tag) == 0 && !empty($current_value)){
-			$parameters[$current_param] = $current_value;
+		if(strcmp($current_param, $tag) == 0 && !empty($current_value))
+		{
+			/* Sometimes parameter might include the namespace e.g.  'loc:serviceID'. The logic below strips off namespace part.*/
+			$position = strpos($current_param, ":");
+			$param_name = $current_param;
+			if( $position !== false) // no namespace character
+			{
+				$param_name = substr($current_param,$position+1); // ignore the ":" - substring from next character after 
+			}
+			
+			//check whether there is a parameter with the same name
+			if(isset($parameters[$param_name])){
+				$repeated_suffix =1; // default parameter
+				//check whether there is a record in the repeated parameters
+				if(isset($repeated_parameters[$param_name]))
+				{
+					$repeated_suffix = $repeated_parameters[$param_name]+1; //assign it to the suffix
+				}
+				
+				$repeated_parameters[$param_name] = $repeated_suffix; // update the repated parameter counter for that value
+				$param_name = $param_name.$repeated_suffix; // append the repeated suffix to the parameter
+			}
+			
+			$parameters[$param_name] = $current_value;
 		}
 	}
 	
@@ -41,7 +68,8 @@
 	/*
 	* The method called when data is encountered.
 	*/
-	function sax_cdata($sax, $data) {
+	function sax_cdata($sax, $data) 
+	{
 		global $current_value;
 		$current_value = trim($data); // copy the tag name into current parameter attribute
 	}
@@ -77,6 +105,7 @@
 	echo $data2;
 	
 	print_r($parameters);
+	print_r($repeated_parameters);
 
 	
 ?>
